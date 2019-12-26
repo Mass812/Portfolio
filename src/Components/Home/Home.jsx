aeda7c97c6fb1eba4d98fccae9a3ee9f27e618a9
mattwellman812 <williamwellman98@gmail.com>
2019-12-26 08:10:17 -0500

    Home Page Color Scheme and Outline

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,25 +2,21 @@ import React from "react";
 import "./Home.scss";
 
 const Home = () => {
-  const pro = require("../../Assets/bp.jpg");
   const matt = require("../../Assets/mattHead.png");
 
   return (
-    <div className="Wrapper">
-      <div className="sp-container">
-        <img src={pro} alt={""} className="pro" />
-        <h1 className="siteTitle">
-          <span>COMPASSION, CHANGE, EXPERIENCE.</span>
-        </h1>
-      </div>
-      <div className="under">
-        <img src={matt} alt={""} className="bobblehead" />
-        <div className='rightContainer'>
-          <h1>Hi, I am Matt Wellman</h1>
-          <h3>Manager, Dad, React Enthusiest</h3>
+    <section className="section">
+      <div className="container">
+        <div className="left">
+          <img src={matt} alt={""} className="bobblehead" />
+        </div>
+        <div className="right">
+          <h1 className="siteTitle1">COMPASSION, CHANGE, EXPERIENCE.</h1>
+          <h1 className="subTitle2">Hi, I am Matt Wellman</h1>
+          <h3 className="subTitle3">Manager, Dad, JavaScript Enthusiest</h3>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
