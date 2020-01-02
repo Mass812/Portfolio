031341749129f0ab2a5be69d25384db3757f6e6d
mattwellman812 <williamwellman98@gmail.com>
2020-01-02 14:11:33 -0500

    jimmied about and project placeholders

--- src/Components/Bio/bio.jsx
+++ src/Components/Bio/bio.jsx
@@ -3,8 +3,43 @@ import "./Bio.scss";
 
 const Bio = () => {
   return (
-    <div>
-      <h5 className="main-title">My Philosophy</h5>
+    <div className="bio-wrapper">
+      <div className="bio-parent">
+        <div className="bio-title-area">
+          <div className="bio-main-title">Short Bio</div>
+          <div className="bio-subtitle">& Hobbies Coming Soon . . . </div>
+        </div>
+        <div>
+          Drawing
+          <hr />
+          <div>Drawing Examples</div>
+        </div>
+        <br />
+        <div>
+          Music
+          <hr />
+          <div>Song Writing / Composing / Singing Examples</div>
+        </div>
+        <br />
+        <div>
+          Reading
+          <hr />
+          <div>BookList</div>
+        </div>
+        <br />
+        <div>
+          Coding
+          <hr />
+          <div>Languages, Frameworks, Favorites</div>
+        </div>
+        <br />
+        <div>
+          Family
+          <hr />
+          <div>The most amazing surprises ever</div>
+        </div>
+        <br />
+      </div>
     </div>
   );
 };
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -5,9 +5,29 @@ import "./Projects.scss";
 
 const Projects = () => {
  
-  return(
-    <h1>Hello</h1>
+return(
+    <div className="project-wrapper">
+      <div className="project-parent">
+        <div className="project-title-area">
+          <div className="project-main-title">Project Bio</div>
+        </div>
+        <div>
+          IMBD Mimic
+        </div>
+        <br/>
+        <div>
+          Amazon Mimic
+        </div>
+        <br />
+        <div>
+          Air BnB Mimic
+        </div>
+        <br />
+      </div>
+    </div>
   );
 };
+
+
 
 export default Projects;
