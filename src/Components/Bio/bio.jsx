f22a52171863841a386a40b749e99ccfbca0c812
mattwellman812 <williamwellman98@gmail.com>
2020-01-04 16:11:17 -0500

    Bio page sketch out

--- src/Components/Bio/bio.jsx
+++ src/Components/Bio/bio.jsx
@@ -11,111 +11,184 @@ import {
   faNode,
   faPython,
   faSass,
-  faSlack,
   faSourcetree,
   faMicrosoft
 } from "@fortawesome/free-brands-svg-icons";
 //import { faCircle } from "@fortawesome/free-regular-svg-icons";
 
 const Bio = () => {
+  const word = require("../../Assets/microsoftWord.png");
+  const excel = require("../../Assets/excel.png");
+  const bullet = require("../../Assets/iconBullet.png");
+
   return (
     <div className="bio-wrapper">
       <div className="bio-parent">
         <div className="bio-title-area">
-          <div className="bio-main-title">Short Bio</div>
-          <div className="bio-subtitle">
+          <div className="bio-subtitle-main">
             High Acuity
-            <div >
+            <div>
               <ul className="icon-splay">
+                <img className="microsoft" src={word} alt="Microsoft Word" />
+                <img className="microsoft" src={excel} alt="Microsoft Excel" />
                 <FontAwesomeIcon
                   icon={faJsSquare}
-                  className="IconBarLi"
+                  className="icon-bar-li-bio"
                   size="lg"
                 />
                 <FontAwesomeIcon
                   icon={faGit}
-                  className="IconBarLi"
+                  className="icon-bar-li-bio"
                   size="lg"
                 />
                 <FontAwesomeIcon
                   icon={faReact}
-                  className="IconBarLi"
+                  className="icon-bar-li-bio"
                   size="lg"
                 />
                 <FontAwesomeIcon
                   icon={faHtml5}
-                  className="IconBarLi"
+                  className="icon-bar-li-bio"
                   size="lg"
                 />
                 <FontAwesomeIcon
                   icon={faJira}
-                  className="IconBarLi"
+                  className="icon-bar-li-bio"
                   size="lg"
                 />
                 <FontAwesomeIcon
                   icon={faNode}
-                  className="IconBarLi"
+                  className="icon-bar-li-bio"
                   size="lg"
                 />
                 <FontAwesomeIcon
                   icon={faPython}
-                  className="IconBarLi"
+                  className="icon-bar-li-bio"
                   size="lg"
                 />
                 <FontAwesomeIcon
                   icon={faSass}
-                  className="IconBarLi"
+                  className="icon-bar-li-bio"
                   size="lg"
                 />
                 <FontAwesomeIcon
                   icon={faSourcetree}
-                  className="IconBarLi"
+                  className="icon-bar-li-bio"
                   size="lg"
                 />
                 <FontAwesomeIcon
                   icon={faLess}
-                  className="IconBarLi"
+                  className="icon-bar-li-bio"
                   size="lg"
                 />
                 <FontAwesomeIcon
                   icon={faMicrosoft}
-                  className="IconBarLi"
+                  className="icon-bar-li-bio"
                   size="lg"
                 />
               </ul>
             </div>
           </div>
         </div>
-        <div>
-          Drawing
-          <hr />
-          <div>Drawing Examples</div>
+
+        <div className="bio-subtitle">
+          Business Characteristics
+          <div className="business-characteristic-parent">
+            <ul className="business-characteristic-ul">
+              <div>
+                <img className="bullet" src={bullet} alt="bullet point" />
+                <span className="bullet-pint-description">
+                  {" "}
+                  High Business Acumen
+                </span>
+              </div>
+              <div>
+                <img className="bullet" src={bullet} alt="bullet point" />
+                <span className="bullet-pint-description">
+                  {" "}
+                  Amazing Ability to Learn Fast
+                </span>
+              </div>
+              <div>
+                <img className="bullet" src={bullet} alt="bullet point" />
+                <span className="bullet-pint-description">
+                  {" "}
+                  Affable and Well Spoken
+                </span>
+              </div>
+              <div>
+                <img className="bullet" src={bullet} alt="bullet point" />
+                <span className="bullet-pint-description">
+                  {" "}
+                  Embrace & Endorse Change
+                </span>
+              </div>
+              <div>
+                <img className="bullet" src={bullet} alt="bullet point" />
+                <span className="bullet-pint-description">
+                  {" "}
+                  Very Strong Work Ethic
+                </span>
+              </div>
+            </ul>
+          </div>
         </div>
-        <br />
+      
         <div>
-          Music
           <hr />
-          <div>Song Writing / Composing / Singing Examples</div>
+          <div className="bio-subtitle">
+            Greatest Weakness & Strength
+            <p>
+              <q>
+                Internally, I am not always an easy personality, I thrive in
+                progress and stagnate in idleness. I am healthiest when I can
+                operate at the edge of my capabilities, pushing myself to learn,
+                commit, do and achieve more. I give myself over so completely at
+                times to my work that I have to literally <i>mentally nudge</i>{" "}
+                myself to back off the throttle enough to enjoy the fruits of
+                those labors.
+              </q>
+            </p>
+            <hr />
+          </div>
         </div>
-        <br />
-        <div>
-          Reading
-          <hr />
-          <div>BookList</div>
+    
+        <div className="bio-subtitle">
+          Certifications
+          <div className="business-characteristic-parent">
+            <ul className="business-characteristic-ul">
+            <hr/>
+              <div>
+                <img className="bullet" src={bullet} alt="bullet point" />
+                <span className="bullet-pint-description">
+                  {" "}
+                  React-Native, JavaScript, Node.js
+                </span>
+              </div>
+
+              <div>
+                <img className="bullet" src={bullet} alt="bullet point" />
+                <span className="bullet-pint-description"> AED, CPR, BBP</span>
+              </div>
+              <div>
+                <img className="bullet" src={bullet} alt="bullet point" />
+                <span className="bullet-pint-description">
+                  {" "}
+                  Yellow-Belt Lean Sigma Six
+                </span>
+              </div>
+              <div>
+                <img className="bullet" src={bullet} alt="bullet point" />
+                <span className="bullet-pint-description">
+                  {" "}
+                  Project Management Certification
+                </span>
+              </div>
+              <hr/>
+            </ul>
+          </div>
+         
         </div>
-        <br />
-        <div>
-          Coding
-          <hr />
-          <div>Languages, Frameworks, Favorites</div>
-        </div>
-        <br />
-        <div>
-          Family
-          <hr />
-          <div>The most amazing surprises ever</div>
-        </div>
-        <br />
       </div>
     </div>
   );
