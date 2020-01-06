836b0ab59486758ca52f36e602811cfcaaedea8b
mattwellman812 <williamwellman98@gmail.com>
2020-01-06 16:36:29 -0500

    no message

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -17,55 +17,69 @@ const Home = () => {
     const headline3 = statement.children[2];
 
     window.gsap
-      .timeline(frame,{ opacity: 0 , duration: 3})
+      .timeline(frame, { opacity: 0, duration: 3 })
 
-      
-
-      .fromTo(img, {
+      .fromTo(
+        img,
+        {
           duration: 2,
           height: 330,
           borderWidth: 10,
-          ease: 'smooth'
-          
-          
+          ease: "smooth"
         },
         {
           delay: 1,
           height: 100,
           borderWidth: 4,
           ease: "Power1.easeIn"
-          
-      }
-      
-      
+        }
       )
-      .fromTo(name, {
-        duration: 1,
-        opacity: 0,
-        ease: "Power1.easeIn"
-      },{
-        opacity: 1
-      })
+      .fromTo(
+        name,
+        {
+          duration: 1,
+          opacity: 0,
+          ease: "Power1.easeIn"
+        },
+        {
+          opacity: 1
+        }
+      )
 
       .from(
         [headline1, headline2, headline3],
-        
-         {
-            stagger: 0.5,
-            delay: .01,
-            opacity: 0,
-            height: 0,
-            fontSize: 3,
-            ease: 'Power4.easeIn',
-            color: 'rgb(210,181,113)'
-          
+
+        {
+          stagger: 0.5,
+          delay: 0.01,
+          opacity: 0,
+          height: 0,
+          fontSize: 3,
+          ease: "Bounce.easeOut",
+          color: "rgb(210,181,113)"
         }
-      
-  
       )
-      .to(name, {duration:1, x: -30, ease: "Power3.easeOut", opacity: 0, delay: 3 })
-      .to([headline1, headline2, headline3], {duration:0.7, y: 300, ease: "bounce.easeOut", opacity: 0, delay: 0 });
+      // .to(name, {
+      //   duration: 1,
+      //   x: -30,
+      //   ease: "Back.easeOut",
+      //   opacity: 0,
+      //   delay: 3
+      // });
+      // .to([headline1, headline2, headline3], {
+      //   duration: 0.7,
+      //   y: 300,
+      //   ease: "bounce.easeOut",
+      //   opacity: 0,
+      //   delay: 0
+      // });
+
+     
   
+  
+
+
+
 
     return () => {
       console.log("cleaned");
