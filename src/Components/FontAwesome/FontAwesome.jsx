aa4b003b591554757778262ad9d6da1903cfc828
mattwellman812 <williamwellman98@gmail.com>
2020-01-13 16:56:35 -0500

    no message

--- src/Components/FontAwesome/FontAwesome.jsx
+++ src/Components/FontAwesome/FontAwesome.jsx
@@ -3,7 +3,7 @@ import React from "react";
 // get our fontawesome imports
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
-import { faCircle } from "@fortawesome/free-regular-svg-icons";
+import { faEnvelopeSquare } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import "./FontAwesome.scss";
@@ -13,27 +13,28 @@ const FontAwesome = () => {
     <section className="contactBlockContainer">
       <div className="contactBlockIconBlock">
         <ul className="IconBar">
-          <i href="https://www.facebook.com/william.m.wellman">
+          <a href="https://www.facebook.com/william.m.wellman">
             <FontAwesomeIcon
               icon={faFacebookF}
               className="IconBarLi"
               size="lg"
             />
-          </i>
-          <i href="https://www.linkedin.com/in/matt-wellman-154301a3?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BmRw6jTXHSQCdaLMn1XAr7A%3D%3D">
+          </a>
+          <a href="https://www.linkedin.com/in/matt-wellman-154301a3?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BmRw6jTXHSQCdaLMn1XAr7A%3D%3D">
             <FontAwesomeIcon
               icon={faLinkedin}
               className="IconBarLi"
               size="lg"
             />
-          </i>
-          <i>
-            <FontAwesomeIcon icon={faCircle} className="IconBarLi" size="lg" />
-          </i>
+          </a >
+          <a href="mailto:https://williamwellman98@gmail.com?subject=subject&cc=cc@example.com">
 
-          <i href="https://github.com/Mass812">
+            <FontAwesomeIcon icon={faEnvelopeSquare} className="IconBarLi" size="lg" />
+          </a>
+
+          <a href="https://github.com/Mass812">
             <FontAwesomeIcon icon={faGithub} className="IconBarLi" size="lg" />
-          </i>
+          </a>
         </ul>
       </div>
     </section>
