f243a845eeac78e68573187ef441c07dfe01fb8e
mattwellman812 <williamwellman98@gmail.com>
2020-01-31 19:03:01 -0500

    no message

--- src/Components/Bio/Intro.jsx
+++ src/Components/Bio/Intro.jsx
@@ -9,18 +9,17 @@ const Intro = () => {
 				<p className='weakness'>
 					<q>
 						I began truly developing in the beginning of 2019, although I was
-						language proficient in JavaScript and Python well beforehand. I
-						wrote a lot of scripts to add functionality to spreadsheets while
-						I was at Techstyle Fashion Group. Beginning this year of 2020 I
-						plan on building out larger scale projects beyond simple demos
-						further increase my proficiency in React as well as other modern
-						JS frameworks.
+						language proficient in JavaScript and Python well beforehand as I
+						wrote a lot of scripts to add functionality to spreadsheets in
+						past. Beginning this year (2020) I plan on building out larger
+						scale projects, beyond simple demos to further increase my proficiency
+						in React as well as other modern JS frameworks.
 					</q>
 				</p>
 				<p className='weakness'>
 					<q>
 						I am Ecma Script 6 & 7 efficient on the Javascript side of things.
-						I use all current practices in React (and React-Native) such as
+						I use current practices in React (and React-Native) such as
 						Hooks, useContext, useReducer, etc. I am always excited by
 						challenges and continually try to learn new ways of doing things
 						because it adds to my ability of being able to approach issues
