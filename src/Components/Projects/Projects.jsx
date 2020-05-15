5de26246eec0a60c3509fe05591be1809dec9091
mattwellman812 <williamwellman98@gmail.com>
2020-05-15 14:27:39 -0400

    no message

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -13,18 +13,18 @@ const Projects = () => {
 	const projectArray = [
 		{
 			id: 1,
-			title: 'ups, Worldport',
+			title: 'Worldport',
 			src: `${upsProject}`,
 			alt: 'project',
 			to: 'https://upssafety-f8d4d.web.app/',
 			body:
-				'UPS Worldport Safety Efficiency Platform is an auditing platform used by Ramp D, UPS Worldport Safety! The audits are conducted through Team monitoring. Counting incidents per event. As such multiple counters were implemented to directly mirror operational paperwork. When I joined the safety committee I saw a great opportunity for react to modernize the old process: paperwork, illegibility and filling out the same paperwork three times per audit seemed really outdated. So I created this web application to handle a brunt of that workload, and allowed for the data  to be more readily available, readable, and useful through the monthly reports components. I feel accomplished in that I saw a problem and used my development skills to make the process better; easier; modern.',
+				'UPS Worldport Safety Efficiency Platform is an auditing platform used by Ramp D, UPS Worldport Safety! The audits are conducted through Team monitoring. Counting incidents per event. As such multiple counters were implemented to directly mirror operational paperwork. When I joined the safety committee I saw a great opportunity for react to modernize the old process: paperwork, illegibility and filling out the same paperwork three times per audit seemed really outdated. So I created this web application to handle a brunt of that workload, and allowed for the data  to be more readily available, readable, and useful through the monthly reports. I feel accomplished in that I saw a problem and used my development skills to make the process better; easier; modern.',
 			details:
 				'Every data value is stored in redux and handled through action generators. The application utilizes React, Redux, Redux-Persist, Firebase Auth and Firestore. Per request I am presenting only the earliest version of this app, to display the demo of the apps functionality. The full site generates emails with document information, creates new rows to spreadsheets, and tracks supervisors safety efficiency over time. The colleted data points show trends and recommend safety outreach and focuses for UPS CHSP and CERT teams. ',
 
 			highlightOne: 'Hosts projects as well as Work History and Recommendations',
 			stack: 'React, React-Router-Dom, Redux, Persist, GSap, SendGrid & FontAwesome',
-			status: 'first build, v-1'
+			status: 'First Build, version 1.0.0 Beta'
 		},
 		{
 			id: 2,
