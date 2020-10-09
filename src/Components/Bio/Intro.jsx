33972179a79ea1539111e3e89b670ab7fa89a42c
mattwellman812 <williamwellman98@gmail.com>
2020-10-09 10:29:31 -0400

    added in react-helmet to project

--- src/Components/Bio/Intro.jsx
+++ src/Components/Bio/Intro.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import './Bio.scss'
+import React from 'react';
+import './Bio.scss';
 
 const Intro = () => {
 	return (
@@ -7,32 +7,17 @@ const Intro = () => {
 			<div className='bio-subtitle'>
 				About Me
 				<div className='weakness'>
-				
-						I began truly developing in the beginning of 2019, although I was language
-						proficient in JavaScript and Python well beforehand as I wrote a lot of
-						scripts to add functionality to spreadsheets in past. Beginning this year
-						(2020) I plan on building out larger scale projects, beyond simple demos to
-						further increase my proficiency in React as well as other modern JS
-						frameworks.
-				
+					I began truly developing in the beginning of 2018, although I was language proficient in JavaScript well beforehand as I wrote a lot of scripts to add functionality to spreadsheets in past. Beginning this year (2020) I plan on building out larger scale projects, beyond simple
+					demos to further increase my proficiency in React as well as other modern JS frameworks.
 				</div>
 				<div className='weakness'>
-				
-						I am Ecma Script 6 & 7 efficient on the Javascript side of things. I use
-						current practices in React (and React-Native) such as Hooks, useContext,
-						useReducer, etc. I am efficient in Redux and its middleware's Thunk and
-						Persist, Firebase, Node & Express. I am always excited by challenges and
-						continually try to learn new ways of doing things because it adds to my
-						ability of being able to approach issues from different perspectives and
-						tackle problems at different angles. I still have a lot to learn but that is
-						what this journey is about.
-				
-
+					I am Ecma Script 6 & 7 efficient on the Javascript side of things. I use current practices in React (and React-Native) such as Hooks, useContext, useReducer, etc. I am efficient in Redux and its middleware Thunk and Persist. I have done MERN projects but by far prefer serverless
+					options such as Firebase as as a backend. I am adept in both Node & Express. I am always excited by challenges and continually try to learn new ways of doing things because it adds to my ability of being able to approach issues from different perspectives and tackle problems at
+					different angles. I still have a lot to learn but that is what this journey is about.
 					<br />
-				
 				</div>
 			</div>
 		</div>
-	)
-}
-export default Intro
+	);
+};
+export default Intro;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect } from 'react';
 import './Home.scss';
 import FontAwesome from '../ContactBar/ContactBar';
+import HelmetMeta from '../HelemetMeta/HelmetMeta';
 
 const Home = () => {
 	const matt = require('../../Assets/mw_black.jpg');
@@ -61,23 +62,28 @@ const Home = () => {
 
 	return (
 		<div>
+			<HelmetMeta
+				titleString={'Matt Wellman Louisville React & React Native Developer'}
+				quoteString={'Making life easier one app at a time.'}
+				bodyString={
+					'I began truly developing in the beginning of 2018, although I was language proficient in JavaScript and Python well beforehand as I wrote a lot of scripts to add functionality to spreadsheets in past. Beginning this year (2020) I plan on building out larger scale projects, beyond simple demos to further increase my proficiency in React as well as other modern JS frameworks. I am Ecma Script 6 & 7 efficient on the Javascript side of things. I use current practices in React (and React-Native) such as Hooks, useContext, useReducer, etc. I am efficient in Redux and its middleware Thunk and Persist, Firebase, Node & Express. I am always excited by challenges and continually try to learn new ways of doing things because it adds to my ability of being able to approach issues from different perspectives and tackle problems at different angles. I still have a lot to learn but that is what this journey is about.'
+				}
+			/>
 			<div className='home-container'>
 				<div className='home-top'>
 					<div className='home-frame' ref={(cr) => (frame = cr)}>
 						<img src={matt} alt={''} className='home-icon' ref={(cr) => (img = cr)} />
 					</div>
-				<div className='home-bottom'>
-					<div className='home-name' ref={(cr) => (name = cr)} style={{ fontSize: 50 }}>
+					<div className='home-bottom'>
+						<div className='home-name' ref={(cr) => (name = cr)} style={{ fontSize: 50 }}>
 							Matt Wellman
-					</div>
-						<div  ref={(cr) => (statement = cr)}>
-
-					<div className='home-subtitle'>JavaScript Enthusiast &</div>
-					<div className='home-subTitle2'>React & React-Native Developer</div>
 						</div>
+						<div ref={(cr) => (statement = cr)}>
+							<div className='home-subtitle'>JavaScript Enthusiast &</div>
+							<div className='home-subTitle2'>React & React-Native Developer</div>
+						</div>
+					</div>
 				</div>
-				</div>
-
 			</div>
 
 			<FontAwesome />
