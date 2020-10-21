0eb4fa5b0c02c5686a4e3e5d0dbf04920a875553
mattwellman812 <williamwellman98@gmail.com>
2020-10-21 09:01:27 -0400

    added phone number and removed facebook

--- src/Components/ContactBar/ContactBar.jsx
+++ src/Components/ContactBar/ContactBar.jsx
@@ -1,44 +1,76 @@
-import React from "react";
+import React, { useEffect, useState, useRef, Fragment } from 'react';
+import { TweenMax } from 'gsap';
 
 // get our fontawesome imports
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelopeSquare } from "@fortawesome/free-solid-svg-icons";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import "./ContactBar.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelopeSquare, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import './ContactBar.scss';
 
 const FontAwesome = () => {
-  return (
-    <section className="contactBlockContainer">
-      <div className="contactBlockIconBlock">
-        <ul className="IconBar">
-          <a href="https://www.facebook.com/william.m.wellman">
-            <FontAwesomeIcon
-              icon={faFacebookF}
-              className="IconBarLi"
-              size="lg"
-            />
-          </a>
-          <a href="https://www.linkedin.com/in/matt-wellman-154301a3?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BmRw6jTXHSQCdaLMn1XAr7A%3D%3D">
-            <FontAwesomeIcon
-              icon={faLinkedin}
-              className="IconBarLi"
-              size="lg"
-            />
-          </a >
-          <a href="mailto:https://williamwellman98@gmail.com?subject=subject&cc=cc@example.com">
+	let phone = useRef(null);
+	let info = useRef(null);
+	const [
+		showNumber,
+		setShowNumber
+	] = useState(null);
 
-            <FontAwesomeIcon icon={faEnvelopeSquare} className="IconBarLi" size="lg" />
-          </a>
+	useEffect(
+		() => {
+			animateContactBar();
+		},
+		[
+			showNumber
+		]
+	);
 
-          <a href="https://github.com/Mass812">
-            <FontAwesomeIcon icon={faGithub} className="IconBarLi" size="lg" />
-          </a>
-        </ul>
-      </div>
-    </section>
-  );
+	const animateContactBar = () => {
+		if (showNumber === null) {
+			return;
+		} else if (!showNumber) {
+			TweenMax.fromTo(info, 1, { opacity: 1, x: 167 }, { x: 0, zIndex: 0 });
+		} else if (showNumber) {
+			TweenMax.fromTo(info, 1, { opacity: 0 }, { x: 167, opacity: 1, zIndex: 20 });
+			TweenMax.fromTo(phone, 1, { zIndex: 10 }, { zIndex: 25 });
+		}
+	};
+
+	return (
+		<Fragment>
+			<section className='contactBlockContainer' ref={(cr) => (phone = cr)}>
+				<div className='contactBlockIconBlock'>
+					<ul className='IconBar'>
+						{/* <a href='https://www.facebook.com/william.m.wellman'>
+							<FontAwesomeIcon icon={faFacebookF} className='IconBarLi' size='lg' />
+						</a> */}
+						<a href='https://www.linkedin.com/in/matt-wellman-154301a3?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BmRw6jTXHSQCdaLMn1XAr7A%3D%3D'>
+							<FontAwesomeIcon icon={faLinkedin} className='IconBarLi' size='lg' />
+						</a>
+						<a href='mailto:williamwellman98@gmail.com?subject=Touchbase'>
+							<FontAwesomeIcon icon={faEnvelopeSquare} className='IconBarLi' size='lg' />
+						</a>
+
+						<a href='https://github.com/Mass812'>
+							<FontAwesomeIcon icon={faGithub} className='IconBarLi' size='lg' />
+						</a>
+						<div>
+							<FontAwesomeIcon icon={faPhone} color={'goldenrod'} className='IconBarLi' size='lg' onClick={() => setShowNumber(!showNumber)} />
+						</div>
+					</ul>
+				</div>
+			</section>
+			<section className='contactBlockContainerMirror' ref={(cr) => (info = cr)}>
+				<div className='contactBlockIconBlock'>
+					<ul className='IconBar-info'>
+						{' '}
+						<FontAwesomeIcon icon={faPhone} color={'green'} style={{ paddingLeft: '7px' }} /> <a href='tel:+15022320232'>(502) 232-0232</a>
+					</ul>
+				</div>
+			</section>
+		</Fragment>
+	);
 };
 
 // render to #root
